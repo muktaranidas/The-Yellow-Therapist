b5e5b5c15e195c340d386a2410729108e33849aa
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-03-20 22:50:25 -0700

    footer done

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <footer className="footer footer-center p-10  text-gray-600">
+        <div>
+          <p> © The Yellow Therapist | All right reserved</p>
+          <p className="">Terms and Conditions | Privacy Policy | Contact us</p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
