08925bb89c8a3896eb88ad549161a04a52af1c92
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-03-20 22:36:32 -0700

    header done

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,7 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import headerImg from "../../../Images/Header/Header-Logo.png";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div class="">
+      <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div class="relative flex items-center justify-between">
+          <div class="flex  items-center">
+            <a
+              href="/"
+              aria-label="Company"
+              title="Company"
+              class="inline-block items-center "
+            >
+              <img src={headerImg} alt="" className="" />
+              <span class="ml-2 text-xl  tracking-wide header-logo ">
+                Because yellow is the happiest color.
+              </span>
+            </a>
+          </div>
+          <ul class="flex  lg:inline-block items-center hidden  ">
+            <li>
+              <a
+                href="/"
+                aria-label="Sign in"
+                title="Sign in"
+                class=" header-right tracking-wide text-black transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Time
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Sign in"
+                title="Sign in"
+                class="header-right text-center tracking-wide text-black  duration-200 hover:text-teal-accent-400"
+              >
+                Date
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                aria-label="Sign in"
+                title="Sign in"
+                class="header-right tracking-wide text-black transition-colors duration-200 hover:text-teal-accent-400"
+              >
+                Guest User
+              </a>
+            </li>
+          </ul>
+
+          <div class="lg:hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                />
+              </svg>
+            </button>
+            {isMenuOpen && (
+              <div class="absolute top-0 left-0 w-full">
+                <div class="p-5 bg-white border rounded shadow-sm">
+                  <div class="flex items-center justify-between mb-4">
+                    <div>
+                      <a
+                        href="/"
+                        aria-label="Company"
+                        title="Company"
+                        class="inline-block items-center "
+                      >
+                        <img src={headerImg} alt="" />
+                        <span class="ml-2 text-xl  tracking-wide header-logo ">
+                          Because yellow is the happiest color.
+                        </span>
+                      </a>
+                    </div>
+                    <div>
+                      <button
+                        aria-label="Close Menu"
+                        title="Close Menu"
+                        class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <path
+                            fill="currentColor"
+                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <nav>
+                    <ul class="space-y-4">
+                      <li>
+                        <a
+                          href="/"
+                          aria-label="Sign in"
+                          title="Sign in"
+                          class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                        >
+                          Sign in
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/"
+                          class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign up"
+                          title="Sign up"
+                        >
+                          Sign up
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
