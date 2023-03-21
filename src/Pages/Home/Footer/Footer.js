5968ea87834fd4676a644af42045c5b421a84705
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-03-20 23:09:01 -0700

    banner done

--- src/Pages/Home/Footer/Footer.js
+++ src/Pages/Home/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer footer-center p-10  text-gray-600">
+      <footer className="footer footer-center p-10  text-gray-6 00">
         <div>
           <p> © The Yellow Therapist | All right reserved</p>
           <p className="">Terms and Conditions | Privacy Policy | Contact us</p>
--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -1,9 +1,10 @@
 import React from "react";
+import bannerImg from "../../../../Images/Banner/BannerImg.png";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner</h2>
+    <div className="my-8">
+      <img src={bannerImg} alt="" className="border border-orange-600" />
     </div>
   );
 };
