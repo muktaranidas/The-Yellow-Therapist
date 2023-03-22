e825020afea32b67b9b9bc237b1902a9cc51e8bc
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-03-22 14:44:47 -0700

    complete about section

--- src/Pages/Home/Home/About/About.js
+++ src/Pages/Home/Home/About/About.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const About = () => {
-  return <div>Anout</div>;
+  return (
+    <div>
+      <section className=" dark:bg-gray-800 dark:text-gray-50">
+        <div className="flex flex-col space-y-4 text-center lg:text-left">
+          <h1 className="text-3xl font-bold text-yellow-500 leading-none">
+            About TYT
+          </h1>
+          <p className="text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            veritatis nam, consequuntur nostrum similique recusandae, id
+            assumenda harum aperiam exercitationem alias eius sapiente, impedit
+            nisi iusto ad reiciendis. Ad, ut. Doloribus consectetur quasi ipsa
+            quo neque culpa blanditiis ducimus recusandae a veritatis optio
+            cumque, in harum ad nam!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
