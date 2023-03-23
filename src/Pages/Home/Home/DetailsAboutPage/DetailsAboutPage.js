86ccefde473691578a1a020f9d9b540999b84f49
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-03-23 15:35:26 -0700

    complete animation

--- src/Pages/Home/Home/DetailsAboutPage/DetailsAboutPage.js
+++ src/Pages/Home/Home/DetailsAboutPage/DetailsAboutPage.js
@@ -1,43 +1,45 @@
 import React from "react";
-import animation from "../../../../Images/Animation/leptop.gif";
+import handshake from "../../../../Images/Animation/Handshake.gif";
+import leptop from "../../../../Images/Animation/leptop.gif";
+import talk from "../../../../Images/Animation/talk.gif";
 
 const DetailsAboutPage = () => {
   return (
     <div>
-      <section className="dark:bg-gray-800  dark:text-gray-100"></section>
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
-        <img src={animation} alt="" className="w-[350px] " />
+      <section className="dark:bg-gray-800 p-0  dark:text-gray-100"></section>
+      <div className="container p-0 m-0   flex flex-col justify-center  mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
+        <img src={handshake} alt="" className="w-[300px] h-[400px] " />
         <div className="flex flex-col justify-center  text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <p className="text-xl text-yellow-500 font-bold  sm:text-3xl">
             A bridge between...
           </p>
-          <p className="mt-6 mb-8 text-md w-4/5 sm:mb-12">
+          <p className="mt-6 mb-8 text-sm w-4/5 sm:mb-12">
             A platform serving both of who seeks professional help for their
             mental well being and the professional therapists who are always
             ready to help. We're providing a smooth bridge between these two.
           </p>
         </div>
       </div>
-      <div className="container flex flex-col  justify-center p-6 mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
+      <div className="container flex flex-col  justify-center  mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
         <div className="flex flex-col justify-center  text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <p className="text-xl text-yellow-500 font-bold  sm:text-3xl">
             A bridge between...
           </p>
-          <p className="mt-6 mb-8 text-md w-4/5 sm:mb-12">
+          <p className="mt-6 mb-8 text-sm w-4/5 sm:mb-12">
             A platform serving both of who seeks professional help for their
             mental well being and the professional therapists who are always
             ready to help. We're providing a smooth bridge between these two.
           </p>
         </div>
-        <img src={animation} alt="" className="w-[350px] " />
+        <img src={leptop} alt="" className="w-[350px] " />
       </div>{" "}
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
-        <img src={animation} alt="" className="w-[350px] " />
+      <div className="container  mt-[-3] flex flex-col justify-center  my-0 mx-auto sm:py-12  lg:flex-row lg:justify-between w-3/4">
+        <img src={talk} alt="" className="w-[350px] " />
         <div className="flex flex-col justify-center  text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
           <p className="text-xl text-yellow-500 font-bold  sm:text-3xl">
             A bridge between...
           </p>
-          <p className="mt-6 mb-8 text-md w-4/5 sm:mb-12">
+          <p className="mt-6 mb-8 text-sm w-4/5 sm:mb-12">
             A platform serving both of who seeks professional help for their
             mental well being and the professional therapists who are always
             ready to help. We're providing a smooth bridge between these two.
